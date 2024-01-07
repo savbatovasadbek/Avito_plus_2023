ea9414b013beaaf22b6f9d47298528dce4f333ac
Samandar Turaev <samandarturayev217@gmail.com>
2024-01-07 22:52:50 +0500

    3

--- src/components/Home/Works/Works.jsx
+++ src/components/Home/Works/Works.jsx
@@ -1,6 +1,7 @@
-import { Box, Grid, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Grid, Image, Text } from "@chakra-ui/react";
 import img1 from "../../../assets/images/googleSheets.png";
-import img2 from "../../../assets/images/cloud.svg";
+import img2 from "../../../assets/images/bg_image_work_box.png";
+import img3 from "../../../assets/images/cloud.svg";
 
 export const Works = () => {
   return (
@@ -23,7 +24,6 @@ export const Works = () => {
           borderStyle="solid"
           borderWidth="1px"
         >
-          1
           <Text color="#171923" fontSize="24px" fontWeight="bold" mb="16px">
             Редактируйте Google Таблицу
           </Text>
@@ -35,12 +35,23 @@ export const Works = () => {
         <Box
           p="32px"
           borderRadius="32"
-          backgroundColor="white"
+          backgroundColor="#6E4EB4"
           borderColor="#FAFAFA"
           borderStyle="solid"
           borderWidth="1px"
+          backgroundImage={`url('${img2}')`}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
         >
-          2
+          <Text
+            color="white"
+            fontSize="28px"
+            textAlign="center"
+            fontWeight={800}
+          >
+            Выгрузка на Авито раз в 60 минут
+          </Text>
         </Box>
         <Box
           p="32px"
@@ -50,7 +61,6 @@ export const Works = () => {
           borderStyle="solid"
           borderWidth="1px"
         >
-          3
           <Text color="#171923" fontSize="24px" fontWeight="bold" mb="16px">
             Авито публикует объявления
           </Text>
@@ -58,7 +68,68 @@ export const Works = () => {
             Если в объявлении ошибка — мы отобразим ее в таблице рядом с
             объявлением
           </Text>
-          <Image src={img2} alt="img." mt="44px" />
+          <Image src={img3} alt="img." mt="44px" />
+        </Box>
+      </Grid>
+      <Grid templateColumns="repeat(2,1fr)" gap="24px" mt="30px">
+        <Box>
+          <Text color="#171923" fontSize="24px" fontWeight="bold" mb="16px">
+            Статистика Авито
+          </Text>
+          <Text color="#2D3748" fontSize="18px" mb="16px">
+            Отслеживайте статистику по объявлениями и Авито аккаунтам.
+            Анализируйте и принимайте решения на основе просмотров, конверсий,
+            лайков и сообщений.
+          </Text>
+          <Button
+            color="purple.500"
+            borderWidth={1}
+            borderColor="purple.500"
+            backgroundColor="white"
+          >
+            Яндекс Диск
+          </Button>
+        </Box>
+        <Box display="flex" justifyContent="center" gap="10px">
+          <Box>
+            <Box w="200px" p="12px" borderRadius={12} backgroundColor="blue">
+              <Box> Просмотры за 7 дней</Box>
+              <Box>👀 783</Box>
+              <Box> Icon 87.09%</Box>
+            </Box>
+
+            <Box
+              w="200px"
+              p="12px"
+              borderRadius={12}
+              backgroundColor="blue"
+              mt="20px"
+              ml="10px"
+            >
+              <Box> Просмотры за 7 дней</Box>
+              <Box>👀 783</Box>
+              <Box> Icon 87.09%</Box>
+            </Box>
+          </Box>
+          <Box mt="20px">
+            <Box w="200px" p="12px" borderRadius={12} backgroundColor="blue">
+              <Box> Просмотры за 7 дней</Box>
+              <Box>👀 783</Box>
+              <Box> Icon 87.09%</Box>
+            </Box>
+            <Box
+              w="200px"
+              p="12px"
+              borderRadius={12}
+              backgroundColor="blue"
+              mt="20px"
+              ml="10px"
+            >
+              <Box> Просмотры за 7 дней</Box>
+              <Box>👀 783</Box>
+              <Box> Icon 87.09%</Box>
+            </Box>
+          </Box>
         </Box>
       </Grid>
     </div>

09ee2fc05552b25f22ccdfb75c26f8837198079e
Samandar Turaev <samandarturayev217@gmail.com>
2024-01-07 20:49:23 +0500

    fix2

--- src/components/Home/ShortView/ShortView.jsx
+++ src/components/Home/ShortView/ShortView.jsx
@@ -1,3 +1,28 @@
+import { Box, Button, Grid, Text } from "@chakra-ui/react";
+
 export function ShortView() {
-  return <div>ShortView</div>;
+  return (
+    <div>
+      ShortView
+      <Grid templateColumns="repeat(2, 1fr)" gap={"106px"} alignItems="center">
+        <Box>
+          <Text color="#171923" fontSize="48px">
+            Краткий обзор
+          </Text>
+          <Text color="#805AD5" fontSize="48px">
+            — 120 секунд
+          </Text>
+          <Text fontSize="18px" mt="32px">
+            Используйте разное время публикации, разные станции метро и адреса,
+            отслеживайте статистику и включайте рекламу —
+          </Text>
+          <Text fontSize="18px" color="#805AD5">
+            Это все не покидая таблицы.
+          </Text>
+          <Button>Начните сейчас</Button>
+        </Box>
+        <Box bg="blue.500"></Box>
+      </Grid>
+    </div>
+  );
 }

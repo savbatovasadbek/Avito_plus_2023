d8783f668d4c44be30a108f5a6580cde503afdcc
savbatovasadbek <asavbatov@gmail.com>
2024-01-07 19:45:33 +0500

    File structure added

---
+++ src/components/Home/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { Text } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <Text fontSize="50px" color="tomato">
+        I'm using a custom font-size value for this text
+      </Text>
+    </div>
+  );
+};
+
+export default Header;
